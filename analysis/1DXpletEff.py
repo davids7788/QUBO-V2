d7f8166a204ee21553d712e4eb60cb9e845c6463
Spataro David <david.spataro@desy.de>
2022-08-18 12:29:07 +0200

    notebook->script

---
+++ analysis/1DXpletEff.py
@@ -0,0 +1,126 @@
+import matplotlib.pyplot as plt
+import ROOT
+from ROOT import *
+import numpy as np
+import sys
+sys.path.append("../src")
+
+ROOT.gROOT.LoadMacro("macros/LuxeStyle.C")
+ROOT.gROOT.LoadMacro("macros/LuxeLabels.C")
+
+SetLuxeStyle()
+
+folder = sys.argv[1]
+gen = "-".join("/".join(folder.split("/")[0:-1]).split("-")[0:-1])
+xi = folder.split("e0gpc_")[1].split("_")[0]
+
+gen_x = np.load(gen + "_gen_xplet_list.npy", allow_pickle=True)
+reco_x = np.load(f"{folder}/reco_xplet_list.npy", allow_pickle=True)
+
+matched = []
+fake = []
+
+for t in reco_x:
+    if len(set(t.particle_ids.values())) == 1:
+        matched.append(t)
+    else:
+        fake.append(t)
+
+print(f"Generated tracks: {len(gen_x)}")
+print(f"Matched tracks: {len(matched)}")
+print(f"Fake tracks: {len(fake)}")
+
+gen_xplets = TH1F('gen xplets', 
+                  'x', 50, 0.05275912, 0.55430088)
+
+matched_xplets = TH1F('matched xplets', 
+                      'x', 50, 0.05275912, 0.55430088)
+
+fake_xplets = TH1F('fake xplets', 
+                   'x', 50, 0.05275912, 0.55430088)
+
+reco_xplets = TH1F('reco xplets', 
+                   'x', 50, 0.05275912, 0.55430088)
+
+h_frame = TH1F('frame', 
+               'x', 50, 0.05275912, 0.55430088)
+
+
+
+for g_x in gen_x:
+    gen_xplets.Fill(g_x.coordinates[0][0])
+
+
+for m_x in matched:
+    matched_xplets.Fill(m_x.coordinates[0][0])
+
+
+for f_x in fake:
+    fake_xplets.Fill(f_x.coordinates[0][0])
+    
+
+for r_x in reco_x:
+    reco_xplets.Fill(r_x.coordinates[0][0])
+    
+Teff_error = TEfficiency(matched_xplets, gen_xplets)
+Tfake_error = TEfficiency(fake_xplets, reco_xplets)
+
+
+gStyle.SetPadTickY(0)   
+canv = TCanvas("example","xplet efficiency ", 800, 600)
+
+h_frame.GetYaxis().SetTitle("fraction of counts")
+h_frame.GetXaxis().SetTitle("x [m]")
+h_frame.Draw()
+
+Teff_error.SetMarkerStyle(8)
+Teff_error.SetMarkerColorAlpha(kBlue, 0.85)
+Teff_error.SetMarkerSize(0.95)
+Teff_error.Draw("PSAME")
+
+Tfake_error.SetMarkerStyle(8)
+Tfake_error.SetMarkerColorAlpha(kRed, 0.85)
+Tfake_error.SetMarkerSize(0.95)
+Tfake_error.Draw("PSAME")
+
+gPad.SetRightMargin(0.13)
+gPad.Draw()
+
+rightmax = gen_xplets.GetMaximum()
+scale = gPad.GetUymax() / rightmax
+
+gen_xplets.SetLineColorAlpha(33, 0.8)
+gen_xplets.SetLineWidth(3)
+gen_xplets.Scale(scale)
+gen_xplets.Draw("HISTSAME")
+
+axis = TGaxis(gPad.GetUxmax(),
+              gPad.GetUymin(), 
+              gPad.GetUxmax(),
+              gPad.GetUymax(),
+              0,
+              rightmax,
+              510,
+              "+L")
+axis.SetTitle("counts")
+axis.SetTitleSize(0.05)
+axis.SetLabelFont(42)
+axis.SetTextFont(42)
+axis.SetTextSize(0.05)
+axis.SetTitleOffset(1.4)
+axis.Draw()
+
+
+LUXELabel(0.75, 0.85);
+
+leg = TLegend(0.55, 0.7, 0.75, 0.9)
+leg.AddEntry(Teff_error, "efficiency", "p")
+leg.AddEntry(Tfake_error, "fake rate", "p")
+leg.AddEntry(gen_xplets, "gen xplets", "l")
+leg.SetBorderSize(0)
+leg.SetFillColor(0)
+leg.SetTextSize(0.04)
+leg.SetHeader(f"#xi = {xi}")
+leg.Draw()
+
+canv.SaveAs(f"{folder}/1Dxplet_efficiency_xi_{xi}.pdf")
---
+++ analysis/2DXpletEff.py
@@ -0,0 +1,138 @@
+import matplotlib.pyplot as plt
+import ROOT
+from ROOT import *
+import numpy as np
+import sys
+sys.path.append("../src")
+
+ROOT.gROOT.LoadMacro("macros/LuxeStyle.C")
+ROOT.gROOT.LoadMacro("macros/LuxeLabels.C")
+
+SetLuxeStyle()
+
+folder = sys.argv[1]
+gen = "-".join("/".join(folder.split("/")[0:-1]).split("-")[0:-1])
+xi = folder.split("e0gpc_")[1].split("_")[0]
+
+gen_x = np.load(gen + "_gen_xplet_list.npy", allow_pickle=True)
+reco_x = np.load(f"{folder}/reco_xplet_list.npy", allow_pickle=True)
+
+matched = []
+fake = []
+
+for t in reco_x:
+    if len(set(t.particle_ids.values())) == 1:
+        matched.append(t)
+    else:
+        fake.append(t)
+
+print(f"Generated tracks: {len(gen_x)}")
+print(f"Matched tracks: {len(matched)}")
+print(f"Fake tracks: {len(fake)}")
+
+gen_xplets = ROOT.TH2F('gen_xplets', 
+                       'x vs y', 100, 0.05275912, 0.55430088, 100, -0.00688128, 0.00688128)
+
+matched_xplets = ROOT.TH2F('gen_xplets', 
+                           'x vs y', 100, 0.05275912, 0.55430088, 100, -0.00688128, 0.00688128)
+
+fake_xplets = ROOT.TH2F('fake_xplets', 
+                        'x vs y', 100, 0.05275912, 0.55430088, 100, -0.00688128, 0.00688128)
+
+reco_xplets = ROOT.TH2F('reco_xplets', 
+                        'x vs y', 100, 0.05275912, 0.55430088, 100, -0.00688128, 0.00688128)
+
+
+
+for g_x in gen_x:
+    gen_xplets.Fill(g_x.coordinates[0][0], 
+                    g_x.coordinates[0][1])
+
+for m_x in matched:
+    matched_xplets.Fill(m_x.coordinates[0][0], 
+                        m_x.coordinates[0][1])
+
+for f_x in fake:
+    fake_xplets.Fill(f_x.coordinates[0][0], 
+                     f_x.coordinates[0][1])
+
+for r_x in reco_x:
+    reco_xplets.Fill(r_x.coordinates[0][0], 
+                     r_x.coordinates[0][1])
+
+
+canv = TCanvas("example1", "xplet efficiency ", 600, 400)
+matched_xplets.Divide(gen_xplets)
+
+matched_xplets.GetYaxis().SetTitle("y [m]")
+matched_xplets.GetXaxis().SetTitle("x [m]")
+
+matched_xplets.Draw("COLZ")
+LUXELabel(0.2, 0.85);
+gPad.SetRightMargin(0.1)
+gPad.Draw()
+
+leg=TLegend(0.2,0.14,0.4,0.4)
+leg.SetBorderSize(0)
+leg.SetFillColor(0)
+leg.SetTextSize(0.04)
+leg.SetHeader("#splitline{#xi = " + xi + "}{xplet efficiency}")
+leg.Draw()
+
+canv.Draw()
+canv.SaveAs(f"{folder}/2Dxplet_efficiency_xi_{xi}.pdf")
+try:
+    canv.Close(); 
+    gSystem.ProcessEvents()
+except NameError:
+    pass
+
+#
+canv2 = TCanvas("example2","fake x-plets ",600,400)
+
+fake_xplets.GetYaxis().SetTitle("y [m]")
+fake_xplets.GetXaxis().SetTitle("x [m]")
+
+fake_xplets.Draw("COLZ")
+LUXELabel(0.2,0.85);
+gPad.SetRightMargin(0.1)
+gPad.Draw()
+
+leg=TLegend(0.2,0.14,0.4,0.4)
+leg.SetBorderSize(0)
+leg.SetFillColor(0)
+leg.SetTextSize(0.04)
+leg.SetHeader("#splitline{#xi = " + xi + "}{fake xplets}")
+leg.Draw()
+
+canv2.Update()
+canv2.Draw()
+canv2.SaveAs(f"{folder}/2Dfake_xplets_xi_{xi}.pdf")
+
+try:
+    canv2.Close(); 
+    gSystem.ProcessEvents()
+except NameError:
+    pass
+##
+
+fake_xplets.Divide(reco_xplets)
+canv3 = TCanvas("example3","xplet fake rate ",600,400)
+
+fake_xplets.GetYaxis().SetTitle("y [m]")
+fake_xplets.GetXaxis().SetTitle("x [m]")
+
+fake_xplets.Draw("COLZ")
+LUXELabel(0.2,0.85);
+gPad.SetRightMargin(0.1)
+gPad.Draw()
+
+leg=TLegend(0.2,0.14,0.4,0.4)
+leg.SetBorderSize(0)
+leg.SetFillColor(0)
+leg.SetTextSize(0.04)
+leg.SetHeader("#splitline{#xi = " + xi + "}{xplets fake rate}")
+leg.Draw()
+
+canv3.Draw()
+canv3.SaveAs(f"{folder}/2Dxplet_fake_rate_xi_{xi}.pdf")
