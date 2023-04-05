f467adbc216ff3ac302044aa6e5be3c26d888b13
davids7788 <david.spataro@gmx.de>
2023-04-05 15:43:40 +0200

    ml solve

--- src/ml_solve.py
+++ src/ml_solve.py
@@ -1,9 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import os
+
 
 from pattern.doublet import Doublet
 from pattern.triplet import Triplet
+from utility.time_tracking import hms_string
 
 from tensorflow.keras import optimizers
 from tensorflow.keras.models import Sequential
@@ -14,94 +17,104 @@ from tensorflow.keras import backend
 
 from sklearn.metrics import roc_curve, auc
 
+# train_data_list = ["10", "11", "12", "13", "14", "15", "16", "17"]
+# val_data_list = ["18", "19"]
+# test_data_list = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09"]
 
-# nicely formatted time string
-def hms_string(sec_elapsed):
-    h = int(sec_elapsed / (60 * 60))
-    m = int((sec_elapsed % (60 * 60)) / 60)
-    s = sec_elapsed % 60
-    return "{}:{:>02}:{:>05.2f}".format(h, m, s)
+xi = 5.0
+train_data_list = ["10", "11"]
+val_data_list = ["18"]
+test_data_list = ["00"]
+
+directory = "../../qubo_ml_datasets/xi_5"
+files_in_directory = os.listdir(directory)
+
+new_folder = f"{directory}/ml_solve_{str(np.random.randint(1e8, 1e9))}"
+print(f"Creating folder: {new_folder}\n")
+os.mkdir(new_folder)
+
+print(f"Starting ml_solve for xi={xi}:")
+
+training_samples = []
+validation_samples = []
+test_samples = []
+
+for folder in files_in_directory:
+    if not os.path.isdir(f"{directory}/{folder}"):
+        continue
+    if folder.split("_")[2][2:] in train_data_list:
+        training_samples.append(f"{directory}/{folder}/triplet_list.npy")
+    if folder.split("_")[2][2:] in val_data_list:
+        validation_samples.append(f"{directory}/{folder}/triplet_list.npy")
+    if folder.split("_")[2][2:] in test_data_list:
+        test_samples.append(f"{directory}/{folder}/triplet_list.npy")
+
+print("\nSample(s) used for training:")
+for sample in training_samples:
+    print(sample)
+print("\nSample(s) used for validation:")
+for sample in validation_samples:
+    print(sample)
+print("\nSample(s) used for validation:")
+for sample in test_samples:
+    print(sample)
 
 
-train_0 = np.load("../ml_folders/triplet_list_train_0.npy", allow_pickle=True)
-train_1 = np.load("../ml_folders/triplet_list_train_0.npy", allow_pickle=True)
-train_2 = np.load("../ml_folders/triplet_list_train_0.npy", allow_pickle=True)
-
-t_samples = [train_0, train_1, train_2]
-
-val = np.load("../ml_folders/triplet_list_val.npy", allow_pickle=True)
-
-test = np.load("../ml_folders/triplet_list_test.npy", allow_pickle=True)
-
-training_sample = []
-training_truth = []
-
-val_sample = []
-val_truth = []
-
-test_sample = []
-test_truth = []
-
-test_CWoLa_feature = []
-test_truth_CWoLa_feature = []
-
-
-for sample in t_samples:
-    for triplet in sample:
-        interactions = list(triplet.interactions.values())
-        if not interactions:
-            interactions.append(0)
-        if not [x for x in interactions if x < 0.0]:
-            interactions.append(-1e10)
-
-        feature_vector = [triplet.doublet_1.hit_1_position[0],
-                          min([x for x in interactions if x < 0]),
-                          max([x for x in interactions if x < 0]),
-                          len([x for x in interactions if x < 0]),
-                          list(triplet.interactions.values()).count(1)]
-        training_sample.append(feature_vector)
-        if triplet.quality > 0:
-            training_truth.append(0)
+def extract_feature_vector(triplet):
+    interactions = list(triplet.interactions.values())
+    connections = [0]
+    conflicts = [0]
+    for value in interactions:
+        if value < 0:
+            connections.append(value)
         else:
-            training_truth.append(1)
+            conflicts.append(value)
+    quality = triplet.quality
 
-for triplet in val:
-    interactions = list(triplet.interactions.values())
-    if not interactions:
-        interactions.append(0)
-    if not [x for x in interactions if x < 0.0]:
-        interactions.append(-1e10)
-    feature_vector = [triplet.doublet_1.hit_1_position[0],
-                      min([x for x in interactions if x < 0]),
-                      max([x for x in interactions if x < 0]),
-                      len([x for x in interactions if x < 0]),
-                      list(triplet.interactions.values()).count(1)]
-    val_sample.append(feature_vector)
-    if triplet.is_correct_match:
-        val_truth.append(1)
-    else:
-        val_truth.append(0)
+    return [triplet.doublet_1.hit_1_position[0],
+            quality,
+            min(connections),
+            max(connections),
+            len(connections) - 1,
+            interactions.count(1)]
 
-for triplet in test:
-    interactions = list(triplet.interactions.values())
-    if not interactions:
-        interactions.append(0)
-    if not [x for x in interactions if x < 0.0]:
-        interactions.append(-1e10)
-    feature_vector = [triplet.doublet_1.hit_1_position[0],
-                      min([x for x in interactions if x < 0]),
-                      max([x for x in interactions if x < 0]),
-                      len([x for x in interactions if x < 0]),
-                      list(triplet.interactions.values()).count(1)]
-    test_sample.append(feature_vector)
-    if triplet.is_correct_match:
-        test_truth.append(1)
-    else:
-        test_truth.append(0)
-    if triplet.quality > 0:
-        test_truth_CWoLa_feature.append(0)
-    else:
-        test_truth_CWoLa_feature.append(1)
+
+train_input_labels = []
+train_input_feature_vector = []
+
+val_input_labels = []
+val_input_feature_vector = []
+
+test_input_labels = []
+test_input_feature_vector = []
+
+
+for train_sample in training_samples:
+    train_file = np.load(train_sample, allow_pickle=True)
+    for t in train_file:
+        train_input_feature_vector.append(extract_feature_vector(t))
+        if t.is_correct_match():
+            train_input_labels.append(1)
+        else:
+            train_input_labels.append(0)
+
+for val_sample in validation_samples:
+    val_file = np.load(val_sample, allow_pickle=True)
+    for t in val_file:
+        val_input_feature_vector.append(extract_feature_vector(t))
+        if t.is_correct_match():
+            val_input_labels.append(1)
+        else:
+            val_input_labels.append(0)
+
+for test_sample in test_samples:
+    test_file = np.load(test_sample, allow_pickle=True)
+    for t in test_file:
+        test_input_feature_vector.append(extract_feature_vector(t))
+        if t.is_correct_match():
+            test_input_labels.append(1)
+        else:
+            test_input_labels.append(0)
 
 
 def create_model(optimizer,
@@ -144,14 +157,12 @@ def create_model(optimizer,
 
 # specifying parameters for optimizer
 LR = 1e-3
-EPOCHS = 75
-DECAY = LR /EPOCHS     # in some cases LR / EPOCHS might be useful
-BETA_1 = 0.8
+EPOCHS = 5
+DECAY = LR / EPOCHS     # in some cases LR / EPOCHS might be useful
+BETA_1 = 0.9
 BETA_2 = 0.99
 AMSGRAD = True
 
-# Preparing k-fold cross validation
-K_FOLDING = 5 # optimal value might be 10, but this needs a lot of time
 
 adam = optimizers.Adam(lr=LR, decay=DECAY, beta_1=BETA_1, beta_2=BETA_2, amsgrad=AMSGRAD)
 
@@ -197,23 +208,23 @@ checkpoint_val_accuracy = \
                     mode='max')
 
 callbacks_list = [checkpoint_val_loss, checkpoint_val_accuracy]
-model_history = classifier.fit(training_sample,
-                               training_truth,
-                               class_weight={0: training_truth.count(1),
-                                             1: training_truth.count(0)},
+model_history = classifier.fit(np.asarray(train_input_feature_vector).astype("float32"),
+                               np.asarray(train_input_labels).astype("float32"),
+                               class_weight={0: train_input_labels.count(1) / len(train_input_labels),
+                                             1: train_input_labels.count(0) / len(train_input_labels)},
                                epochs=EPOCHS,
                                verbose=2,
-                               validation_data=(val_sample,
-                                                val_truth),
+                               validation_data=(np.asarray(val_input_feature_vector).astype("float32"),
+                                                np.asarray(val_input_labels).astype("float32")),
                                callbacks=callbacks_list,
                                shuffle=True)
 
 end = time.time()
 
 model_json = classifier.to_json()
-with open('model.json', 'w') as json_file:
+with open(f"{new_folder}/model.json", 'w') as json_file:
     json_file.write(model_json)
-classifier.save_weights('after_last_epoch.h5')
+classifier.save_weights(f"{new_folder}/after_last_epoch.h5")
 
 
 # Plot training & validation accuracy values
@@ -224,7 +235,7 @@ plt.title('Model accuracy')
 plt.ylabel('Accuracy')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='best')
-plt.savefig('model_history-model accuracy')
+plt.savefig(f"{new_folder}/model_history-model accuracy")
 plt.show()
 
 # Plot training & validation loss values
@@ -234,29 +245,24 @@ plt.plot(model_history.history['val_loss'])
 plt.title('Model loss')
 plt.ylabel('Loss')
 plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='best')
-plt.savefig('model_history-model accuracy')
+plt.legend(['Train', 'Val'], loc='best')
+plt.savefig(f"{new_folder}/model_history-model accuracy")
 plt.show()
 
 print(f'elapsed time: {hms_string(end - start)}')
 
-json_file = open('model.json', 'r')
+json_file = open(f"{new_folder}/model.json", "r")
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 
 # load weights into new model
 
-loaded_model.load_weights('weights_best_val_loss.h5')
-predictions = loaded_model.predict(test_sample)
-fpr, tpr, thresholds = roc_curve(test_truth,
-                                 predictions[:, 1])
+loaded_model.load_weights(f"{new_folder}/weights_best_val_loss.h5")
+predictions = loaded_model.predict(train_input_feature_vector)
+fpr, tpr, thresholds = roc_curve(test_input_feature_vector,
+                                 test_input_labels[:, 1])
 roc_auc = auc(fpr, tpr)
-
-predictions_CWoLa_feature = loaded_model.predict(test_sample)
-fpr_CWoLa_feature, tpr_CWoLa_feature, thresholds_CWoLa_feature = \
-    roc_curve(test_truth_CWoLa_feature,  predictions_CWoLa_feature[:, 1])
-roc_auc_CWoLa_feature = auc(fpr_CWoLa_feature, tpr_CWoLa_feature)
 
 plt.figure()
 plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r', label='Chance', alpha=.8)
@@ -266,14 +272,7 @@ plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
 plt.title('ROC-Curve')
 plt.legend(loc="lower right")
-plt.savefig('ROC_Curve')
-
-
-# Training data
-
-plt.plot(fpr_CWoLa_feature, tpr_CWoLa_feature, color='b',
-         label=r'CWoLa feature separation (AUC = %0.2f)' % roc_auc_CWoLa_feature,
-         lw=2, alpha=.8)
+plt.savefig(f"{new_folder}/ROC_Curve")
 
 # Evaluation data
 
